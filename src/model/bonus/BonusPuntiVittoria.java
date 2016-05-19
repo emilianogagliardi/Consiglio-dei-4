7ee621932c18ffcdab6f472837c93c2b99b01867
emilianogagliardi <emilianogagliardi@icloud.com>
2016-05-19 09:37:01 +0200

    vediamo se va a posto

--- src/model/bonus/BonusPuntiVittoria.java
+++ src/model/bonus/BonusPuntiVittoria.java
@@ -1,4 +1,15 @@
 package model.bonus;
 
 public class BonusPuntiVittoria extends RealBonus {
+    private int puntiVittoria;
+
+    public BonusPuntiVittoria(int p, RealBonus decorated) throws IllegalArgumentException{
+        super(decorated);
+        if (p <= 0) throw new IllegalArgumentException("Non è possibile creare bonus punti vittoria con un numero negativo o nullo di punti");
+        puntiVittoria = p;
+    }
+
+    public void ottieni(Giocatore giocatore) {
+        giocatore.guadagnaPuntiVittoria(puntiVittoria);
+    }
 }
