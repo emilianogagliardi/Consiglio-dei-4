f0d7c166b84835642296873bed198528639f6a01
Riccardo Bertoglio <ticio94@hotmail.it>
2016-05-23 21:53:08 +0200

    Classe Mazzo eseguibile

--- src/model/Mazzo.java
+++ src/model/Mazzo.java
@@ -1,26 +1,31 @@
 package model;
 
 import model.carte.Carta;
+
+import java.util.AbstractQueue;
 import java.util.Collection;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.LinkedBlockingQueue;
 import static java.util.Arrays.asList;
 import static java.util.Collections.shuffle;
 
 public class Mazzo <T extends Carta> {
-    private Collection<T> mazzo = new LinkedBlockingQueue<T>(); //l'interfaccia LinkedBlockingQueue implementa BlockingQueue che estende Queue. Queue rappresenta una FIFO.
+    private AbstractQueue<T> mazzo = new LinkedBlockingQueue<T>(); //l'interfaccia LinkedBlockingQueue implementa BlockingQueue che estende Queue. Queue rappresenta una FIFO.
     //BlockingQueue è un'interfaccia che rende bloccante la FIFO: è bloccato il recupero di un elemento fino a che la pila diventa non vuota.
 
-    public void addCarta(T carta) {
+    public void addCarta(T carta){
         mazzo.add(carta);
     }
 
-    public void addCarte(List<T> carte) {
+    public void addCarte(AbstractQueue<T> carte) {
         mazzo.addAll(carte);
     }
 
-    public T getCarta() {
-        return mazzo.remove(mazzo.element());
+    public T getCarta() throws NoSuchElementException { //NoSuchElementException viene lanciata quando non ci sono carte nel mazzo
+        T cartaDaRestituire = mazzo.element();
+        mazzo.remove(cartaDaRestituire);
+        return cartaDaRestituire;
     }
 
     public void mischia(){
