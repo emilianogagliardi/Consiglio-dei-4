f18509fef7c09e3fa687ad2eda4a573262d8d6a1
emilianogagliardi <emilianogagliardi@icloud.com>
2016-06-14 12:18:54 +0200

    altri resources aggiunti

--- src/client/view/GUI/FlussoView.java
+++ src/client/view/GUI/FlussoView.java
@@ -15,13 +15,13 @@ public class FlussoView extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException{
         finestraAttuale = primaryStage;
-        setFinestraDaFXML("login.fxml");
+        setFinestraDaFXML("mappegallery.fxml");
     }
 
     public void setFinestraDaFXML(String nomeFile) throws IOException{
         //assegna la scena
         FXMLLoader loader = new FXMLLoader();
-        Parent root =  loader.load(getClass().getResource(nomeFile).openStream());
+        Parent root =  loader.load(getClass().getResource("/"+nomeFile).openStream());
         GestoreFlussoFinestra controllerFX = loader.getController();
         controllerFX.setFlusso(this);
         finestraAttuale.setTitle("Council Of Four");
